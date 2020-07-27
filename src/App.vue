3f48933aa453268963e9d97d34285cd892be02db
Reshetnikov A <user@mail.ru>
2020-07-27 13:15:44 +0400

    Блок 5. Фиксация. Урок 6.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h2 v-colored:background.font="'red'">{{title}}</h2><hr />
+    <h2 v-colored:color.delay.font="'blue'">{{title}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{
+        title: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
