2373c84f2de765ff2d09251e20348f0c8f0bf801
Reshetnikov A <user@mail.ru>
2020-07-27 16:29:37 +0400

    Блок 7. Работы с RadioButton. Урок 4.

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,18 @@
     <h2>Form inputs</h2>
 
     <label>
-      <input type="checkbox" value="instagram" v-model="social" /> Instagram
+      <input type="radio" value="instagram" v-model="social" /> Instagram
     </label><hr />
 
     <label>
-      <input type="checkbox" value="vk" v-model="social" /> Vk
+      <input type="radio" value="vk" v-model="social" /> Vk
     </label><hr />
 
     <label>
-      <input type="checkbox" value="facebook" v-model="social" /> Facebook
+      <input type="radio" value="facebook" v-model="social" /> Facebook
     </label><hr />
 
-    <ol>
-      <li v-for="s in social" v-bind:key="s.id">{{s}}</li>  
-    </ol>
+    <p>{{social}}</p>
 
   </div>
 </template>
@@ -25,7 +23,7 @@
   export default {
     data(){
       return{
-        social: ["vk"]
+        social: "vk"
       }
     }
   }
