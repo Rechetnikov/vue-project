ace29b8ed028ce3820a762a69129b7ed3c4086a3
Reshetnikov A <user@mail.ru>
2020-07-31 09:36:17 +0400

    Валидация пустота и email

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="container">
-    <form class="pt-3">
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" id="email" class="form-control"  />
-        <!-- v-model="email" -->
-      </div>
-    </form>
+    <div class="form-group">
+      <label class="form_label">Name</label>
+      <input 
+        class="form-control" 
+        v-model.trim.lazy="$v.name.$model" 
+        :class="{'is-invalid': $v.name.$error }" 
+      />
+    </div>
+    <div v-if="$v.name.$dirty">
+      <div class="alert alert-danger" v-if="!$v.name.required">Field is required</div>
+      <div class="alert alert-danger" v-if="!$v.name.email">Invalid email</div>
+    </div>
   </div>
 </template>
 
 <script>
+  import {required, email} from 'vuelidate/lib/validators'
   export default {
-    // data(){
-    //   email: '123'
-    // }
+    data() {
+      return {
+        name: ''
+      }
+    },
+    validations: {
+      name: {
+        required,
+        email
+      }
+    },
   }
 </script>
