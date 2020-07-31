b0eeb70fa954c29d0f8b96bfc604e699c8866c88
Reshetnikov A <user@mail.ru>
2020-07-31 12:17:39 +0400

    Установка роутера. Блок 9. Урок 1.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.use(Vuelidate)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
