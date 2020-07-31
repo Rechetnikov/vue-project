e33414ca211f7b5c741993018a24535ec9b63d5b
Reshetnikov A <user@mail.ru>
2020-07-31 12:07:59 +0400

    Настрой сабмит у формы с валидацией

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,56 @@
 <template>
   <div class="container">
-    <div class="form-group">
-      <label class="form_label">E-mail</label>
-      <input 
-        class="form-control w-25" 
-        v-model.trim.lazy="$v.name.$model" 
-        :class="{'is-invalid': $v.name.$error }" 
-      />
-      <div class='my-3 w-25' v-if="$v.name.$dirty">
-        <div class="alert alert-danger" v-if="!$v.name.required">Field is required</div>
-        <div class="alert alert-danger" v-if="!$v.name.email">Invalid email</div>
-        <div class="alert alert-danger" v-if="!$v.name.uniqEmail">Not unique</div>
-      </div>      
-      <!-- <pre>{{$v.name}}</pre> -->
-      
-    </div>
-    <div class="form-group">
-      <label class="form_label">Password</label>
-      <input 
-      type="password"
-        class="form-control w-25" 
-        v-model.trim.lazy="$v.password.$model" 
-        :class="{'is-invalid': $v.password.$error }" 
-      />
-      <div class='my-3 w-25' v-if="$v.password.$dirty">
-        <div class="alert alert-danger" v-if="!$v.password.required">Field is required</div>
-        <div class="alert alert-danger" v-if="!$v.password.minLength">Field must have at least {{$v.password.$params.minLength.min}} characters.</div>
+    <form class="pt-3" @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label class="form_label">E-mail</label>
+        <input 
+          class="form-control w-25" 
+          v-model.trim.lazy="$v.name.$model" 
+          :class="{'is-invalid': $v.name.$error }" 
+          readonly onfocus="this.removeAttribute('readonly')"
+        />
+
+        <div class='my-3 w-25' v-if="$v.name.$dirty">
+          <div class="alert alert-danger" v-if="!$v.name.required">Field is required</div>
+          <div class="alert alert-danger" v-if="!$v.name.email">Invalid email</div>
+          <div class="alert alert-danger" v-if="!$v.name.uniqEmail">Not unique</div>
+        </div>      
+        
       </div>
-    </div>
-    <div class="form-group">
-      <label class="form_label">Confirm password</label>
-      <input 
-      type="password"
-        class="form-control w-25" 
-        v-model.trim.lazy="$v.confirm.$model" 
-        :class="{'is-invalid': $v.confirm.$error }" 
-      />
-      <div class='my-3 w-25' v-if="$v.confirm.$dirty">
-        <div class="alert alert-danger" v-if="!$v.confirm.required">Field is required</div>
-        <div class="alert alert-danger" v-if="!$v.confirm.sameAsPassword">Passwords must be identical.</div>
+      <div class="form-group">
+        <label class="form_label">Password</label>
+        <input 
+        type="password"
+          class="form-control w-25" 
+          v-model.trim.lazy="$v.password.$model" 
+          :class="{'is-invalid': $v.password.$error }" 
+          readonly onfocus="this.removeAttribute('readonly')"
+        />
+        <div class='my-3 w-25' v-if="$v.password.$dirty">
+          <div class="alert alert-danger" v-if="!$v.password.required">Field is required</div>
+          <div class="alert alert-danger" v-if="!$v.password.minLength">Field must have at least {{$v.password.$params.minLength.min}} characters.</div>
+        </div>
       </div>
-    </div>    
+      <div class="form-group">
+        <label class="form_label">Confirm password</label>
+        <input 
+        type="password"
+          class="form-control w-25" 
+          v-model.trim.lazy="$v.confirm.$model" 
+          :class="{'is-invalid': $v.confirm.$error }" 
+          readonly onfocus="this.removeAttribute('readonly')"
+        />
+        <div class='my-3 w-25' v-if="$v.confirm.$dirty">
+          <div class="alert alert-danger" v-if="!$v.confirm.required">Field is required</div>
+          <div class="alert alert-danger" v-if="!$v.confirm.sameAsPassword">Passwords must be identical.</div>
+        </div>
+      </div>
+      <button 
+        class="btn btn-success"
+        type='submit'
+        :disabled = "$v.$invalid"
+      >Submit</button>
+    </form>
   </div>
 </template>
 
@@ -53,6 +63,12 @@
         password: '',
         confirm: ''
       }
+    },
+    methods:{
+      onSubmit(){
+        console.log('Email', this.name);
+        console.log('Password', this.password);
+      },
     },
     validations: {
       name: {
