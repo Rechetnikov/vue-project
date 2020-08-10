e9ea67df654da68126a5a32def3716b9cf323acf
Reshetnikov A <user@mail.ru>
2020-08-10 15:47:00 +0400

    Vuex store Modules. Блок 11. Урок 6

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import counter from './counter'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        counter: 0
-    },
-    mutations: {
-        changeCounter(state, payload) {
-            state.counter += payload
-        }
-    },
-    actions: {
-        asyncChangeCounter({ commit }, payload) {
-            setTimeout(() => {
-                commit('changeCounter', payload.counterValue);
-            }, payload.timeoutDelay);
-        }
-    },
-    getters: {
-        computedCounter(state) {
-            return state.counter * 10
-        }
+    modules: {
+        counter
     }
 })
