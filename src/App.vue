c46d57affc733b9f5d6c77e5e2d85d13b300a768
Reshetnikov A <user@mail.ru>
2020-08-10 15:09:32 +0400

    Vuex и работа с getters. Блок 11. Урок 3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container text-center pt-5">
     <app-counter></app-counter>
+    <app-second-counter></app-second-counter>
     <app-actions></app-actions>
   </div>
 </template>
 
 <script>
 import Counter from "./Counter";
+import SecondCounter from "./SecondCounter";
 import Actions from "./Actions";
 
 export default {
   components: {
     appCounter: Counter,
     appActions: Actions,
+    appSecondCounter: SecondCounter,
   },
 };
 </script>
